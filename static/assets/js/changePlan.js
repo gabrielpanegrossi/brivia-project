31e3cba3ef241bf24271091315bcdbccd03e1fba
gabrielpanegrossi <gabriel7751535@gmail.com>
2021-11-11 03:27:26 -0300

    fixed indentation

--- static/assets/js/changePlan.js
+++ static/assets/js/changePlan.js
@@ -5,27 +5,29 @@ $(document).ready(function () {
 	hideItems();
 });
 
-$( window ).resize(function() {
-	hideItems()
+$(window).resize(function () {
+	hideItems();
 });
 
 $('.rightArrow-icon').click(() => {
 	changeOption(htmlElementsObj);
 });
 
-function hideItems(){
+function hideItems() {
 	const renderedArr = $('.plans-illustrator .nav-pills .nav-item');
 
 	renderedArr.map((index, component) => {
-		index > ($('#pills-tab-plans').width() > 300 ? 1 : 0) ? component.style.display = "none" : component.style.display = "initial" 
+		index > ($('#pills-tab-plans').width() > 300 ? 1 : 0)
+			? (component.style.display = 'none')
+			: (component.style.display = 'initial');
 	});
 }
 
-function createHtmlArr(obj){
+function createHtmlArr(obj) {
 	const htmlArr = Object.values(obj);
 
 	htmlArr.push(htmlArr[0]);
-	htmlArr.splice(0,1);
+	htmlArr.splice(0, 1);
 
 	return htmlArr;
 }
@@ -35,15 +37,14 @@ function changeOption(obj) {
 	plansContainer.innerHTML = '';
 	let content = '';
 
-
 	arr.map((index, component) => {
 		obj[index] = component.outerHTML;
 	});
 
 	const finalArr = createHtmlArr(obj);
 
-	finalArr.map((component, index ) => {
-		 content += component;
+	finalArr.map((component, index) => {
+		content += component;
 	});
 
 	plansContainer.innerHTML = content;
